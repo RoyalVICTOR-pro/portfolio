96122a1d01a44d078b445966a155aa4c0874efbf
Royal VICTOR <tech@steewo.io>
2024-11-01 15:46:03 +0100

    wip : Header

---
+++ components/Navigation/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-row w-screen justify-between h-12 items-center">
+    <div class="bg-red-400"></div>
+    <div class="">
+      <nav>
+        <ul class="flex flex-row space-x-6">
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Projets</a></li>
+          <li><a href="#">Recommandations</a></li>
+          <li><a href="#">Perso</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="bg-sky-600">
+      
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
