a96255f6c0c8aa72cb2e454ddbe3f234fb2e752e
Royal VICTOR <tech@steewo.io>
2024-12-05 15:38:28 +0100

    wip : contact form debug 7

--- server/utils/Mailer.ts
+++ server/utils/Mailer.ts
@@ -9,6 +9,8 @@ export class Mailer {
 
   constructor() {
     const config = useRuntimeConfig()
+    console.log('Création du client Mailgun')
+    console.log('Clé:', config.receiverEmail)
 
     this.mailgun = new Mailgun(formData).client({
       username: 'api',
